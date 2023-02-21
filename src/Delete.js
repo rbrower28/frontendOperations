21d42c7adf63a2e0426e970345f2eb87aa66d3cd
Ryan <ryan.brower28@gmail.com>
2023-02-21 16:04:55 -0700

    CRD operations

--- src/Delete.js
+++ src/Delete.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
+import Axios from 'axios';
 
 export default function Delete() {
+  
+  const [id, set_id] = useState("");
+
+  const sendDelete = () => {
+    const url = `https://subitt-application-clientdemo.onrender.com/client/${id}`;
+    Axios.delete(url).then(() => {
+      console.log("Delete request successful.")
+    })
+  };
+
   return (
     <div>
       <h3>Delete</h3>
+      <form>
+        <label>ID:<input type='text' onChange={(event) => {set_id(event.target.value)}} /></label>
+        <button type='button' onClick={sendDelete}>Delete</button>
+      </form>
     </div>
-    );
+  );
 }
--- src/Update.js
+++ src/Update.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
+import Axios from 'axios';
 
 export default function Update() {
+
+  const [firstName, set_fname] = useState("");
+  const [lastName, set_lname] = useState("");
+  const [email, set_email] = useState("");
+
   return (
     <div>
       <h3>Update</h3>
+      <form>
+        <label>ID:
+          <input type='text' />
+        </label>
+        <label>First Name
+          <input type='text' onChange={(event) => {set_fname(event.target.value)}} />
+        </label>
+        <label>Last Name
+        <input type='text' onChange={(event) => {set_lname(event.target.value)}} />
+          </label>
+        <label>Email
+          <input type='text' onChange={(event) => {set_email(event.target.value)}} />
+        </label>
+        {/* <button type='button' onClick={sendUpdate}>Create</button> */}
+      </form>
     </div>
     );
 }
